bfbb772df2deec060e268b1158913606e966efcd
Davonchy <detekamba@gmail.com>
2022-12-26 07:44:44 -0800

    square updated

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,9 +10,9 @@ void print_square(int size)
 int height, width;
 if (size > 0)
 {
-for (height = 0; height <= size; height++)
+for (height = 0; height < size; height++)
 {
-for (width = 0; width <= size; width++)
+for (width = 0; width < size; width++)
 _putchar('#');
 if (height == size - 1)
 continue;
