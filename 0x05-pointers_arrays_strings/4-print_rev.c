8796753ee813738fde68bdba1d7b8008846e878d
Davonchy <detekamba@gmail.com>
2023-01-01 03:08:16 -0800

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_rev - prints a string, in reverse, followed by a new line.
@@ -11,6 +12,6 @@ int length = 0, index;
 while (s[index++])
 length++;
 for (index = length - 1; index >= 0; index--)
-_putchar(s[index]);
-_putchar('\n');
+putchar(s[index]);
+putchar('\n');
 }
