9c57bd2ee421798588b44263c1966f1d8ea3af74
Davonchy <detekamba@gmail.com>
2023-05-02 10:29:52 -0700

    double digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,30 +7,20 @@
  */
 int main(void)
 {
-int num1;
-int num2;
-int num3;
-int num4;
-for (num1 = 0; num1 < 10; num1++)
+int num1, num2;
+for (num1 = 0; num1 <= 98; num1++)
 {
-for (num2 = 0; num2 <= 10; num2++)
+for (num2 = 0; num2 <= 99; num2++)
 {
-for (num3 = 0; num3 < 10; num3++)
-{
-for (num4 = 0; num4 < 10; num4++)
-{
-putchar('0' + num1);
-putchar('0' + num2);
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
 putchar(32);
-putchar('0' + num3);
-putchar('0' + num4);
-if (!(num1 == 9 && num2 == 8))
-{
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+if (num1 == 98 && num2 == 99)
+continue;
 putchar(',');
 putchar(32);
-}
-}
-}
 }
 }
 putchar('\n');
