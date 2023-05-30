a867f879a4a5960e7717aa3417a4827afc50c2f1
Davonchy <detekamba@gmail.com>
2023-05-30 06:05:04 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,13 +15,16 @@ int num;
 for (num = 1; num <= 100; num++)
 {
 if ((num % 3) == 0 && (num % 5) == 0)
-printf("FizzBuzz ");
+printf("FizzBuzz");
 else if ((num % 3) == 0)
-printf("Fizz ");
+printf("Fizz");
 else if ((num % 5) == 0)
-printf("Buzz ");
+printf("Buzz");
 else
-printf("%d ", num);
+printf("%d", num);
+if (num == 100)
+continue;
+printf(" ");
 }
 printf("\n");
 return (0);
