023f321030e27ea91cfd2332f2760f373fbb1aa3
Davonchy <detekamba@gmail.com>
2023-06-23 03:11:24 -0700

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,8 +10,12 @@
 void print_array(int *a, int n)
 {
 int i;
-int j = n - 1;
-for (i = 0; i <= j; i++)
-printf("%d, ", a[i]);
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i == n - 1)
+continue;
+printf(", ");
+}
 printf("\n");
 }
