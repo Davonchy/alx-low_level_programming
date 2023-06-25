a3dca16080033f495a75967bb0feef094f52056f
Davonchy <detekamba@gmail.com>
2023-06-25 14:52:37 -0700

    comparison

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,11 +14,10 @@
 int _strcmp(char *s1, char *s2)
 {
 int i = 0;
-int num = s1[i] - s2[i];
 for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 {
 if (s1[i] != s2[i])
 break;
 }
-return (num);
+return (s1[i] - s2[i]);
 }
