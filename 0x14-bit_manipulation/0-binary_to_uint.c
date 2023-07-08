54c88fad79f3f63475d62d174f9dccbea7551e72
Davonchy <detekamba@gmail.com>
2023-07-08 11:01:24 -0700

    binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,19 +13,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-int i = 0;
-unsigned int sum = 0;
-unsigned int dec = 1;
-int len = strlen(b);
-if (b[i] == '\0')
-return (0);
-for (i = (len - 1); i >= 0; i--)
-{
-if (b[i] != '0' && b[i] != '1')
-return (0);
-else if (b[i] == '1')
-sum += dec;
-dec *= 2;
-}
-return (sum);
+	unsigned int num = 0, mult = 1;
+	int len = 0;
+
+	if (b[len] == '\0')
+		return (0);
+
+	for (len = 0; b[len];)
+		len++;
+
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		num += (b[len] - '0') * mult;
+		mult *= 2;
+	}
+
+	return (num);
 }
