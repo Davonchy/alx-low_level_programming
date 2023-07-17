2890bc52ea9e54889c14c9bceaba63642ea95468
Davonchy <detekamba@gmail.com>
2023-07-17 11:50:42 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,20 +9,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd;
-int count;
-int mode;
-if (filename == NULL)
-return (-1);
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-if (fd == -1)
-return (-1);
-if (!text_content)
-text_content = "";
-for (count = 0; text_content[count] != '\0'; count++)
-mode = write(fd, text_content, count);
-if (mode == -1)
-return (-1);
-close(fd);
-return (1);
+	int err, len, fd;
+
+	err = len = fd = 0;
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+		if (fd < 0)
+			return (-1);
+	while (text_content && text_content[len])
+		len++;
+
+	err = write(fd, text_content, len);
+	if (err < 0)
+		return (-1);
+	close(fd);
+	return (1);
 }
