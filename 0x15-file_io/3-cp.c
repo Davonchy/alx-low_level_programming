9ba606789670f6fb20d348431dab5fb9dd7862fb
Davonchy <detekamba@gmail.com>
2023-07-24 03:34:38 -0700

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,7 +28,7 @@ return (buffer);
 void close_file(int fd)
 {
 int c;
-c = closed(fd);
+c = close(fd);
 if (c == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
@@ -52,8 +52,8 @@ int main(int argc, char *argv[])
 {
 int from;
 int to;
-int read;
-int write;
+int rea;
+int writ;
 char *buffer;
 
 if (argc != 3)
@@ -61,13 +61,13 @@ if (argc != 3)
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-buffer = create_buffer(argv[2]);
+buffer = make_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
+	rea = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (from == -1 || r == -1)
+		if (from == -1 || rea == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
@@ -75,8 +75,8 @@ buffer = create_buffer(argv[2]);
 			exit(98);
 		}
 
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		writ = write(to, buffer, rea);
+		if (to == -1 || writ == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
@@ -84,10 +84,10 @@ buffer = create_buffer(argv[2]);
 			exit(99);
 		}
 
-		r = read(from, buffer, 1024);
+		rea = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (r > 0);
+	} while (rea > 0);
 
 	free(buffer);
 	close_file(from);
