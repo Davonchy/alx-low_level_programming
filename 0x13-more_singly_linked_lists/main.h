d126f257eff31cbcde928947f2953fb02263ad76
Davonchy <detekamba@gmail.com>
2023-07-26 02:18:05 -0700

    list

---
+++ 0x13-more_singly_linked_lists/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+
+#endif
