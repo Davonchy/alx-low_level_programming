e5e12d26e675c9c883c8493d653ac1c981a637d5
Davonchy <detekamba@gmail.com>
2023-09-06 02:55:19 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,13 +12,14 @@ int append_text_to_file(const char *filename, char *text_content)
 int fd, w, index = 0;
 if (!filename)
 return (-1);
-else if (!text_content || text_content[0])
-return (1);
+if (text_content != NULL)
+{
+for (index = 0; text_content[index];)
+index++;
+}
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
-while (text_content[index])
-index++;
 w = write(fd, text_content, index);
 if (w == -1)
 return (-1);
